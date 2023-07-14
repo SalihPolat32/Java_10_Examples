f2f4a867a64c36b06cb1ff801f5ce93e75fc627f
Salih Polat <97470567+SalihPolat32@users.noreply.github.com>
2023-07-14 13:19:24 +0300

    14.07.2023 Gün Sonu

---
+++ JavaJDBCExample/src/main/java/com/bilgeadam/utility/ConnectionProvider.java
@@ -0,0 +1,50 @@
+package com.bilgeadam.utility;
+
+import lombok.Getter;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Getter
+public class ConnectionProvider {
+
+    private final String USERNAME = "postgres";
+    private final String PASSWORD = "123456789";
+    private final String DB_NAME = "java10FutbolDb";
+    private final String URL = "jdbc:postgresql://localhost:5432/" + DB_NAME;
+    private Connection connection;
+
+    public boolean openConnection(){
+
+        try {
+
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Bağlantı Başarılı");
+            return true;
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+            System.out.println("Bağlantı Başarısız!!!");
+            return false;
+
+        }
+    }
+
+    public void closeConnection(){
+
+        try {
+
+            if (!connection.isClosed()){
+                connection.close();
+                System.out.println("Bağlantı Sonlandırıldı...");
+            }
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+
+        }
+    }
+}
